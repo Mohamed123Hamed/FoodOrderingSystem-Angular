0c81e401fd5196133e3e194d572c3acbd1e68527
Mohamed123Hamed <mo7hamed17698@gmail.com>
2025-07-10 21:04:34 +0300

    add to cart

--- src/app/pages/cart-component/cart-component.component.ts
+++ src/app/pages/cart-component/cart-component.component.ts
@@ -1,28 +1,42 @@
-import { Component } from '@angular/core';
-import { Router, RouterLink } from '@angular/router';
-import { NgFor } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { OrderService } from '../../shared/services/order.service';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-cart-component',
-  imports: [RouterLink],
+  standalone: true,
+  imports: [RouterLink,NgFor],
   templateUrl: './cart-component.component.html',
   styleUrl: './cart-component.component.css'
 })
-export class CartComponentComponent {
+export class CartComponentComponent implements OnInit {
   cart: any[] = [];
   reservation: any = {};
   totalPrice: number = 0;
-  itemName : string ='';
 
   constructor(private orderService: OrderService) {}
 
   ngOnInit() {
     this.cart = this.orderService.cart;
     this.reservation = this.orderService.reservation;
-    this.totalPrice = this.orderService.totalPrice;
-    this.itemName = this.orderService.itemName;
+    this.calculateTotal();
   }
 
+  increaseQuantity(item: any) {
+    item.quantity += 1;
+    this.calculateTotal();
+  }
 
+  decreaseQuantity(item: any) {
+    if (item.quantity > 1) {
+      item.quantity -= 1;
+      this.calculateTotal();
+    }
+  }
+
+  calculateTotal() {
+    this.totalPrice = this.cart.reduce((sum, item) => sum + item.price * item.quantity, 0);
+    this.orderService.totalPrice = this.totalPrice;
+  }
 }
